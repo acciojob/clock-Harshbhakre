f3c503096d8598a554c0ffc3362555462b58c91c
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-22 11:38:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+let timer = document.querySelector("#timer")
+let currentTime = new Date();
+setInterval(()=>{
+	timer.innerHTML = currentTime.toLocalString()
+},1000)
